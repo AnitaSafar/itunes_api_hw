7706c9303131c16dda4d5fcc0b6bb28ca02cdfd9
Anita Safar <ancsa0416@gmail.com>
2021-11-24 18:26:45 +0000

    app and songlist components created

--- itunes/src/App.js
+++ itunes/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import SongList from './containers/SongList';
 import './App.css';
+
 
 function App() {
   return (
-    
-    
+    <>
+    <h1>Current Top 20 songs - UK </h1>
+    <SongList/>
+    </>
   );
 }
 
---
+++ itunes/src/containers/SongList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SongList = () => {
+
+    return(
+        <h2>Song list</h2>
+    )
+};
+
+export default SongList;
