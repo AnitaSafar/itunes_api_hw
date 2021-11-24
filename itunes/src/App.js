b7f95b72dca5fcdc7bba8139630345f873880f27
Anita Safar <ancsa0416@gmail.com>
2021-11-24 18:47:42 +0000

    data requested

--- itunes/src/App.js
+++ itunes/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SongList from './containers/SongList';
 import './App.css';
 
 
 function App() {
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    getSongs();
+  }, [])
+
+  const getSongs = function(){
+    fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json')
+    .then(response => response.json())
+    .then(songs => setSongs(songs))
+    console.log(songs)
+  }
+
   return (
     <>
     <h1>Current Top 20 songs - UK </h1>
